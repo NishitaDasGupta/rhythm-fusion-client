bc1abd73edc38eec5f75ce18ce85cc4a591508ae
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-06-09 02:50:03 +0600

    ins details

--- src/Pages/Home/Classes/Classes.jsx
+++ src/Pages/Home/Classes/Classes.jsx
@@ -22,13 +22,32 @@ const Classes = () => {
                     {
                         classes.map(classList =>
                             <div key={classList.classId}>
-                               <h1 className="text-[#18adc0] pl-4 py-3 text-2xl font-bold">Course of {classList.classNam}</h1>
-                                <div className="card card-side bg-base-100 shadow-2xl">
-                               <div>
-                               
-                                    <figure><img className="w-[300px] h-[180px] pl-4" src={classList.classImage} alt="Movie" /></figure>
-                               </div>
-                                
+                                <h1 className="text-[#18adc0] pl-4 py-3 text-2xl font-bold">Course of {classList.classNam}</h1>
+                                <div className={classList.availableSeats !== 0 ?
+                                    "card card-side bg-base-100 shadow-2xl" : "card card-side bg-red-500 shadow-2xl"}>
+                                    <div>
+
+                                        <figure><img className="w-[300px] h-[180px] pl-4 pt-3" src={classList.classImage} alt="Movie" /></figure>
+                                    </div>
+                                    <div className="card-body text-xs">
+                                        <h2 className={`card-title text-base ${classList.availableSeats !== 0 ? "text-amber-600" : "text-white"}`}>Ins. {classList.instructorName}</h2>
+                                        <p>Total Students: {classList.numStudents}</p>
+                                        <p>Available Seats: {classList.availableSeats}</p>
+                                        <p>Price: ${classList.price}.00</p>
+                                        {/* button disable or not */}
+
+                                        <div className="card-actions justify-end">
+                                            {classList.availableSeats !== 0
+                                                ?
+                                                <button disabled={false} className="btn  bg-[#1ed8f0] hover:bg-[#1bc2d8]   
+                                            ">Select</button>
+                                                :
+                                                <button disabled={true} className="btn">Select</button>
+                                            }
+
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>)
                     }
--- src/Pages/Home/Instructors/Instructors.jsx
+++ src/Pages/Home/Instructors/Instructors.jsx
@@ -1,10 +1,50 @@
-
+import { Link } from "react-router-dom";
+import musicImg from "../../../assets/loggedin.jpg"
+import useInstructors from "../../../hook/useInstructors";
 
 const Instructors = () => {
+    const [instructors] = useInstructors();
     return (
         <div>
-            inst
-        </div>
+            <div className="hero h-48" style={{ backgroundImage: `url(${musicImg})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl text-white font-bold ">Instructors</h1>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="max-w-6xl mx-auto">
+                <div className="grid grid-cols-2 gap-6 my-14">
+
+                    {
+                        instructors.map(teacher =>
+                            <div key={teacher.instructorId}>
+
+                                <div
+                                    className="card card-side bg-base-100 shadow-2xl">
+                                    <div className="avatar">
+                                        <div className="w-44 rounded">
+                                            <img src={teacher.instructorImage} />
+                                        </div>
+                                    </div>
+                                    <div className="card-body text-xs">
+                                        <h2 className="card-title text-base">Ins. {teacher.name}</h2>
+                                        <p>Email: {teacher.email}</p>
+                                        <p>Total Classes: {teacher.classesTaken}</p>
+                                        <div className="card-actions justify-start">
+                                            <Link to="/classes" className="btn  bg-[#1ed8f0] hover:bg-[#1bc2d8]   
+                                            ">See Classes</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>)
+                    }
+                </div>
+            </div> </div>
+
     );
 };
 
