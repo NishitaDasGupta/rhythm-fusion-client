1c4c988da95247a80868f2345a22921820c33a11
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-06-10 15:19:29 +0600

    env

--- src/Config/firebase.config.js
+++ src/Config/firebase.config.js
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCzt7e0Z8LHP60iINtYxGHx90cndK9jzP4",
-    authDomain: "rhythm-fusion-client.firebaseapp.com",
-    projectId: "rhythm-fusion-client",
-    storageBucket: "rhythm-fusion-client.appspot.com",
-    messagingSenderId: "1027695373349",
-    appId: "1:1027695373349:web:eaead179f0ecd0bd4263cc"
+    // apiKey: "AIzaSyCzt7e0Z8LHP60iINtYxGHx90cndK9jzP4",
+    // authDomain: "rhythm-fusion-client.firebaseapp.com",
+    // projectId: "rhythm-fusion-client",
+    // storageBucket: "rhythm-fusion-client.appspot.com",
+    // messagingSenderId: "1027695373349",
+    // appId: "1:1027695373349:web:eaead179f0ecd0bd4263cc"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
